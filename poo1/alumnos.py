f47ac28e8ceb8e3c6980a6096b752a79d0884a91
adayperdomo <adayperdomorodriguez@gmail.com>
2024-02-28 12:36:52 +0000

    imprimir nota

--- poo1/alumnos.py
+++ poo1/alumnos.py
@@ -2,3 +2,8 @@ class alumno:
     def __init__(self, nombre, nota):
         self.nota = nota
         self.nombre = nombre
+
+    def imprimir(self):
+        print(self.nombre, "tiene un:", self.nota)
+
+    
