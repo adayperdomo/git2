9888f5d5dcfc5bd12436f15cb78cd42ff1861a94
adayperdomo <adayperdomorodriguez@gmail.com>
2024-02-28 12:47:00 +0000

    creación 2

--- poo1/main_.py
+++ poo1/main_.py
@@ -1,9 +1,1 @@
-from alumno import alumno
-
-def main():
-    dani = alumno("dani", 19)
-    dani.imprimir()
-    dani.promociona()
-
-if __name__ == "__main__":
-    main
+from persona import persona
