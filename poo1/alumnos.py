56ca694f2c3dd8683dffd7507a7a4dd4347be2e6
adayperdomo <adayperdomorodriguez@gmail.com>
2024-02-28 12:40:08 +0000

    promociona

--- poo1/alumnos.py
+++ poo1/alumnos.py
@@ -6,4 +6,9 @@ class alumno:
     def imprimir(self):
         print(self.nombre, "tiene un:", self.nota)
 
-    
+    def promociona(self):
+        if (self.nota >= 5):
+            print(self.nota, "promociona")
+        else:
+            (self.nombre, "no promociona")
+
