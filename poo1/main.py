f3954e27c4f6ac3808feb949bdf2eed4f3f5c84e
adayperdomo <adayperdomorodriguez@gmail.com>
2024-02-28 09:54:14 +0000

    mover

---
+++ poo1/main.py
@@ -0,0 +1,22 @@
+from persona import persona
+
+def main():
+    juan = persona("juan", 15)
+    juan.imprimir()
+    juan.cumpleanos()
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+from alumno import alumno
+
+def main():
+    daniel = alumno("daniel", 5)
+    daniel.imprimir()
+    daniel.promociona()
+
+if __name__ == "__main__":
+    main()
+"""
