4f66deaeaad5e1bbee7632e25b77fb4c6462f380
adayperdomo <adayperdomorodriguez@gmail.com>
2024-02-28 09:47:57 +0000

    juan

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from alumno import alumno
+from persona import persona
 
 def main():
-    daniel = alumno("daniel", 5)
-    daniel.imprimir()
-    daniel.promociona()
+    juan = persona("juan", 15)
+    juan.imprimir()
+    juan.promociona()
 
 if __name__ == "__main__":
     main()
