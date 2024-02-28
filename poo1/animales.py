f97b267e298730e04c0c000f8187644fb3b41160
adayperdomo <adayperdomorodriguez@gmail.com>
2024-02-28 12:30:03 +0000

    vaca

--- poo1/animales.py
+++ poo1/animales.py
@@ -3,3 +3,9 @@ class animal:
     def caminar(self):
         self.patas = 0 
         print("caminando con", self.patas, "patas")
+
+def main():
+    vaca = animal()
+    vaca.patas = 4
+    vaca.caminar()
+    
