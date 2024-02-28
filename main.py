a77296946506b27f687eba9442b197372885ce72
adayperdomo <adayperdomorodriguez@gmail.com>
2024-02-28 09:41:37 +0000

    listo

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+"""
+from alumno import alumno
+
+def main():
+    daniel = alumno("daniel", 5)
+    daniel.imprimir()
+    daniel.promociona()
+
+if __name__ == "__main__":
+    main()
+"""
